d2405f1677bc030cb88f5412b5d2f5f61a188d34
aditya <2018aditya.tare@ves.ac.in>
2021-02-03 20:26:01 +0530

    make changes

--- js/app.js
+++ js/app.js
@@ -13,11 +13,11 @@ $(document).ready(function(){
   element: document.getElementById('exp-id'),
   handler: function() {
     var p = document.querySelectorAll('.progress-bar');
-    p[0].setAttribute("style", "width:90%;transition:1s all");
-    p[1].setAttribute("style", "width:95%;transition:1.5s all");
-    p[2].setAttribute("style", "width:85%;transition:1.7s all");
-    p[3].setAttribute("style", "width:75%;transition:2s all");
-    p[4].setAttribute("style", "width:78%;transition:2.3s all");
+    p[0].setAttribute("style", "width:95%;transition:1s all");
+    p[1].setAttribute("style", "width:85%;transition:1.5s all");
+    p[2].setAttribute("style", "width:90%;transition:1.7s all");
+    p[3].setAttribute("style", "width:50%;transition:2s all");
+    p[4].setAttribute("style", "width:30%;transition:2.3s all");
   },
   offset:'90%'
 });
